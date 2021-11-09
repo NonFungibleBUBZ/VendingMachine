c37be4af818b89ff8a2b2ce0cd81df22ba07da78
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-09 18:32:48 -0300

    Update logWalletAddress.js

--- server/test/logWalletAddress.js
+++ server/test/logWalletAddress.js
@@ -1,11 +1,13 @@
 const { getFakeWallets } = require("./utils");
+const { cardanocliJs } = require( "../utils/cardano" );
 
 const logWalletAddresses = function () {
-	const wallets = getFakeWallets();
+	/*const wallets = getFakeWallets();
 
 	for (let i = 0; i < wallets.length; i++) {
 		console.log(JSON.stringify(wallets[i].paymentAddr, null,4));
-	}
+	}*/
+	console.log(cardanocliJs.wallet('testWallet').paymentAddr)
 };
 
 logWalletAddresses();
