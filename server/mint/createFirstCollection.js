c8c66c629262c290d6b9a94a75a2e20f80ebe0f7
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-10 16:41:49 -0300

    Update createFirstCollection.js

--- server/mint/createFirstCollection.js
+++ server/mint/createFirstCollection.js
@@ -1,11 +1,12 @@
 const db = require('./db')
-
 const metadataArray = require('./metadata/metadata_first_collection')
-
 const  mintController  = require('./controllers/mintController')
+const { cardanocliJs } = require( "../utils/cardano" );
 
 let create = async function () {
-    mintController.mintAsset(metadataArray[0], 25123456, 'addr_test1qru8huxjk8wna7aq8edp0w6dqkk7060jeruece5u233l8z27rh9l5kr8ewvl2xfm342dqnwkc4rdruzremf2v9yugysswrm7p4')
+    let x = cardanocliJs.transactionSubmit(
+    mintController.mintAsset(metadataArray[0], 25123456, 'addr_test1qru8huxjk8wna7aq8edp0w6dqkk7060jeruece5u233l8z27rh9l5kr8ewvl2xfm342dqnwkc4rdruzremf2v9yugysswrm7p4'))
+    console.log(x)
 }
 
 try {
