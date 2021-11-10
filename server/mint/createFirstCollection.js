7cb350136fae51c348520c49bbb4bc2c556a5d24
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-10 15:58:27 -0300

    Update createFirstCollection.js

--- server/mint/createFirstCollection.js
+++ server/mint/createFirstCollection.js
@@ -1,7 +1,8 @@
 const db = require('./db')
 
 let create = async function () {
-   await db.update_collection('firstCollection')
+   await db.set_unavailable(0,'firstCollection')
+    await db.update_collection('firstCollection')
 }
 
 try {
