dcc35eae6652e46c0ff9857296e10d329e9b74c5
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-10 16:27:38 -0300

    Update createFirstCollection.js

--- server/mint/createFirstCollection.js
+++ server/mint/createFirstCollection.js
@@ -5,7 +5,7 @@ const metadataArray = require('./metadata/metadata_first_collection')
 const  mintController  = require('./controllers/mintController')
 
 let create = async function () {
-    mintController.mintAsset(metadataArray[0], 25123456, '')
+    mintController.mintAsset(metadataArray[0], 25123456, 'addr_test1qru8huxjk8wna7aq8edp0w6dqkk7060jeruece5u233l8z27rh9l5kr8ewvl2xfm342dqnwkc4rdruzremf2v9yugysswrm7p4')
 }
 
 try {
