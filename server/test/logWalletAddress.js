71da06e6b73f90f498561864aa60ef07be3fc770
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-10 16:27:03 -0300

    Update logWalletAddress.js

--- server/test/logWalletAddress.js
+++ server/test/logWalletAddress.js
@@ -8,7 +8,7 @@ const logWalletAddresses = function () {
 	const wallets = getFakeWallets();
 
 	for (let i = 0; i < wallets.length; i++) {
-		console.log(JSON.stringify(wallets[i].paymentAddr(), null,4));
+		console.log(JSON.stringify(wallets[i].paymentAddr, null,4));
 
 	}
 };
