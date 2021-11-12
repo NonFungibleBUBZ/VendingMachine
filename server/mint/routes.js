1dd1d45573e53f99f7616dce5887e375a6b78a29
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-12 12:08:26 -0300

    Update routes.js

--- server/mint/routes.js
+++ server/mint/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { mintController } = require('./controllers/mintController')
+const { autoMintHandler } = require('./controllers/autoMint')
 const controller = require('./controller');
 const db = require('./db');
 
@@ -8,8 +8,6 @@ router.get ('/', (req,res) => {
     return res.status(200).json({"message":"report working"});
 })
 
-console.log(mintController)
-
-router.get("/mint", mintController);
+router.get("/mint", autoMintHandler);
 
 module.exports = router;
