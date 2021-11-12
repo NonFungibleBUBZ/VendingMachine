9deff8b58ce3ac29cac2b57db1260891a613ec1f
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-12 16:38:56 -0300

    Update createFirstCollection.js

--- server/mint/createFirstCollection.js
+++ server/mint/createFirstCollection.js
@@ -3,10 +3,54 @@ const metadataArray = require('./metadata/metadata_first_collection')
 const  mintController  = require('./controllers/mintController')
 const { cardanocliJs } = require( "../utils/cardano" );
 
-let wallet = cardanocliJs.wallet('fake-wallet-0')
+let sender = cardanocliJs.wallet('fake-wallet-0')
 
 let create = async function () {
-    console.log(wallet.balance())
+    console.log(sender.balance())
+    //72a347b015f5da23a00e5208f58bbff3c5a17f623386337308a5709f.SuperC4405
+
+    const receiver = "addr_test1qru8huxjk8wna7aq8edp0w6dqkk7060jeruece5u233l8z27rh9l5kr8ewvl2xfm342dqnwkc4rdruzremf2v9yugysswrm7p4"
+    const txInfo = {
+        txIn: cardanocliJs.queryUtxo(sender.paymentAddr),
+        txOut: [
+            {
+                address: sender.paymentAddr,
+                value: {
+                    lovelace: sender.balance().value.lovelace - cardanocliJs.toLovelace(1.5)
+                }
+            },
+            {
+                address: receiver,
+                value: {
+                    lovelace: cardanocliJs.toLovelace(1.5),
+                    "72a347b015f5da23a00e5208f58bbff3c5a17f623386337308a5709f.SuperC4405": 1
+                }
+            }
+        ]
+    }
+
+    console.log(txInfo);
+
+    const raw = cardanocliJs.transactionBuildRaw(txInfo)
+
+    const fee = cardanocliJs.transactionCalculateMinFee({
+        ...txInfo,
+        txBody: raw,
+        witnessCount: 1
+    })
+
+    txInfo.txOut[0].value.lovelace -= fee
+
+    const tx = cardanocliJs.transactionBuildRaw({ ...txInfo, fee })
+
+    const txSigned = cardanocliJs.transactionSign({
+        txBody: tx,
+        signingKeys: [sender.payment.skey]
+    })
+
+    const txHash = cardanocliJs.transactionSubmit(txSigned)
+
+    console.log(txHash)
 }
 
 try {
