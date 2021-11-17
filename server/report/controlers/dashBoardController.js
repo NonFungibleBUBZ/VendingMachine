c693ba509c7003ec20b60d6036356ccf492904f9
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-17 18:50:41 -0300

    Update dashBoardController.js

--- server/report/controlers/dashBoardController.js
+++ server/report/controlers/dashBoardController.js
@@ -4,9 +4,11 @@ const { get_availableBubz } = require( "../controller" );
 const reportHandler = async function (req, res) {
 
     let returnAvailablebubz = await get_availableBubz
-    res
-        .status(200)
-        .json({ message: "report test", data: JSON.stringify(returnAvailablebubz) });
+    setTimeout( ()=> {
+        res
+            .status(200)
+            .json({ message: "report test", data: JSON.stringify(returnAvailablebubz) });
+    },0)
 }
 
 module.exports = { reportHandler }
