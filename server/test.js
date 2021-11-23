8a3adbda468bf49947296868556b4ab5530422e3
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-23 14:48:27 -0300

    Update test.js

--- server/test.js
+++ server/test.js
@@ -4,12 +4,12 @@ const run = async function() {
     const sender = cardanocliJs.wallet('fake-wallet-0');
 
     const txInfo = {
-        txIn: [sender.balance().utxo[1]],
+        txIn: [sender.balance().utxo[9]],
         txOut: [
             {
                 address: sender.paymentAddr,
                 value: {
-                    lovelace: ( +sender.balance().utxo[ 1 ].value.lovelace - +cardanocliJs.toLovelace( 25 ) )
+                    lovelace: ( +sender.balance().utxo[ 9 ].value.lovelace - +cardanocliJs.toLovelace( 25 ) )
             }
             },
             {
