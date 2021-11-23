a4b419a1abb1aa7528c17c431bbbeafb0c0ce996
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-23 14:07:03 -0300

    Update test.js

--- server/test.js
+++ server/test.js
@@ -1,7 +1,39 @@
 const { cardanocliJs } = require("./utils/cardano");
 
 const run = async function() {
-    console.log(cardanocliJs.wallet('fake-wallet-0').balance())
+    const sender = cardanocliJs.wallet('fake-wallet-0');
+
+    const txInfo = {
+        txIn: [0],
+        txOut: [
+            {
+                address: cardanocliJs.wallet('testWallet'),
+                value: {
+                    lovelace: cardanocliJs.toLovelace(25),
+                },
+            },
+        ],
+    };
+
+    const raw = cardanocliJs.transactionBuildRaw(txInfo);
+
+    const fee = cardanocliJs.transactionCalculateMinFee({
+        ...txInfo,
+        txBody: raw,
+        witnessCount: 1,
+    });
+
+    txInfo.txOut[0].value.lovelace -= fee;
+
+    const tx = cardanocliJs.transactionBuildRaw({ ...txInfo, fee });
+
+    const txSigned = cardanocliJs.transactionSign({
+        txBody: tx,
+        signingKeys: [sender.payment.skey],
+    });
+
+    const txHash = cardanocliJs.transactionSubmit(txSigned);
+    console.log(txHash)
 }
 
 run()
