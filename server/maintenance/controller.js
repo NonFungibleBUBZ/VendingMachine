169d40ee7b2974f5c6faa5f55256a9c4b108488e
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-29 18:43:21 -0300

    Update controller.js

--- server/maintenance/controller.js
+++ server/maintenance/controller.js
@@ -1,11 +1,11 @@
-const { cardano } = require('../utils/cardano.js')
+const { cardanocliJs } = require("../utils/cardano");
 
 // made in 11/29/2021 by LRovaris -> Github or contact me in Discord LRovaris#4065
 // this script should set things easier
 
-const tokenPrice = cardano.toLovelace(25); // in order to modify tokenPrice just edit the value in ADA
-const fusionPrice = cardano.toLovelace(25) // same for fusion
-const amountToDev = cardano.toLovelace(1) // in order to be 0 there's additional needing to remove the part from the txOut build (for both mint and fuse txOut) in ../mint/autoMint.js
+const tokenPrice = cardanocliJs.toLovelace(25); // in order to modify tokenPrice just edit the value in ADA
+const fusionPrice = cardanocliJs.toLovelace(25) // same for fusion
+const amountToDev = cardanocliJs.toLovelace(1) // in order to be 0 there's additional needing to remove the part from the txOut build (for both mint and fuse txOut) in ../mint/autoMint.js
 const percentToCharity = 0.4 // 1 = 100% so 0.4 == 40% //please avoid % too low, if its less than 1 ADA it might return error building the transaction
 const isCharity = false // that sets the charity drop :3
 const dropWallet = 'addr1q97e2wqsyr74nyevdwcq9e2wfwj05h7kr5py5rdmu5afzewp5ap2lz9pvc56knkmfdun4mlymyfe3hvty7vwzmuvyces57l40z'
